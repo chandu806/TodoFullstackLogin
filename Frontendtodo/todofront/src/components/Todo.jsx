c814cdd2b850af08d4563180113094ab545649e3
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-11-23 22:48:16 +0530

    jk

--- Frontendtodo/todofront/src/components/Todo.jsx
+++ Frontendtodo/todofront/src/components/Todo.jsx
@@ -14,14 +14,90 @@ export const Todo = () => {
     // </div>
     <div className='main'>
         <div id="main2">
-            <input type={text} placeholder="Enter Todo" itemID='title'></input>
+            <input type={text} placeholder="Enter Todo" id='title'></input>
         </div>
         <button onClick={post()} id="addbutoon">ADD</button>
         <div id='contain'>
-            
+
         </div>
     </div>
 
+    const getting=async()=>{
+        try {
+            let res= await fetch(`http://localhost:3000/tasks`);
+            let data= await res.json();
+            console.log(data);
+             
+             update(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getting();
+    
+    
+    const post=async()=>{
+        
+        try {
+            let tasks={
+            title:document.getElementById('title').value,
+        
+            }
+        
+            if(tasks.title.length!==0){
+                let res=await fetch(`http://localhost:3000/tasks`,{
+                    method:"POST",
+                    body:JSON.stringify(tasks),
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                
+                });
+                let data= await res.json();
+        
+            
+            }else{
+            alert("Please fill")
+            }
+        
+        } catch (error) {
+            console.log(error);
+        }
+    
+        
+    }
+    
+    
+    function update(data){
+        data.forEach(function(text){
+            console.log(text);
+            let div=document.createElement('div');
+    
+            div.setAttribute('id',"row1")
+            let tr=document.createElement('h3');
+            let tr1=document.createElement('button');
+            let tr2=document.createElement('button')
+            tr.setAttribute('id',"row2")
+            if(text.check===true){
+                tr.style.color="gray";
+            }else{
+                tr.style.color="orange";
+            }
+            
+            
+            tr.innerText=text.title;
+            tr1.innerText="Edit";
+            tr2.innerText="Delete"
+    
+            tr1.style.color="green";
+            tr2.style.color="red";
+            div.append(tr);
+            tr1.setAttribute('id',"tr1");
+            tr2.setAttribute("id","tr2");
+            document.querySelector('#contain').append(div,tr1,tr2);
+    
+        });
+    }
 
 
   )
--- Frontendtodo/todofront/src/routes/AllRoutes.jsx
+++ Frontendtodo/todofront/src/routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import { Registration } from '../components/Registration'
 import Login from '../components/Login'
 import HomeRoute from './HomeRoute'
+import { Todo } from '../components/Todo'
 
 const AllRoutes = () => {
     return (
@@ -11,6 +12,7 @@ const AllRoutes = () => {
                 <Route path="/:id" element={<HomeRoute />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Registration />} />
+                <Route path="/todo" element={<Todo />} />
             </Routes>
         </div>
     )
